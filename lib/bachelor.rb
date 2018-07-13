b824a2f78da88a79910667eccc75f36415ffeee2
Rosa Tu <rosalina.tu@gmail.com>
2018-07-13 13:26:47 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,23 +1,42 @@
 require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
-  if data[season].values.include?("Winner")
-    binding.pry
-     data[season]["name"].split[0]
-  end   
-  
+    data[season].each do |name|
+    if name["status"] == "Winner"
+    return name["name"].split(" ")[0]
+    end    
+    end  
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, hash|
+    hash.each do |person|
+    if person["occupation"] == occupation
+    return person["name"]
+    end
+    end
+    end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-end
-
+  counter = 0
+  data.each do |season, hash|
+    hash.each do |person|
+      if person["hometown"] == hometown
+      counter += 1
+      end
+    end
+  end
+  return counter
+end  
+  
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, hash|
+    hash.each do |person|
+      if person["hometown"] == hometown
+        return person["name"]
+      end
+    end
+  end  
 end
 
 def get_average_age_for_season(data, season)
